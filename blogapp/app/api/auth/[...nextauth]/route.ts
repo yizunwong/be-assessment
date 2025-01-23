00e3c6b52ac06501dcd79ebf5c7b6062ce332d0f
YiZun0123 <yizun.wong@aserious.co>
2025-01-23 10:51:42 +0800

    fix

--- blogapp/app/api/auth/[...nextauth]/route.ts
+++ blogapp/app/api/auth/[...nextauth]/route.ts
@@ -23,7 +23,7 @@ const authOptions: AuthOptions = {
        * Otherwise, it returns an object with the user's id, name, email, and token.
        */
       async authorize(credentials) {
-        const response = await fetch("http://localhost:3000/api/users/login", {
+        const response = await fetch("/api/users/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
